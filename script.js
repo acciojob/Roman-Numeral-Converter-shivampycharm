de8acb2c1a7d9e7c6ded17627ee990c603289c98
Shivam Dubey <72991053+shivampycharm@users.noreply.github.com>
2023-01-11 23:39:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function convertToRoman(num) {
 
-	const obj = {
+	// const obj = {
+ //        M: 1000,
+ //        CM: 900,
+ //        D: 500,
+ //        CD: 400,
+ //        C: 100,
+ //        XC: 90,
+ //        L: 50,
+ //        XL: 40,
+ //        X: 10,
+ //        IX: 9,
+ //        V: 5,
+ //        IV: 4,
+ //        I: 1
+ //    };
+
+ //    let result = "";
+ //    for (const symbol in obj) {
+ //        while (decimal >= obj[symbol]) {
+ //            result += symbol;
+ //            decimal -= obj[symbol];
+ //        }
+ //    }
+ //    return result;
+
+	const lookup = {
         M: 1000,
         CM: 900,
         D: 500,
@@ -17,10 +42,10 @@ function convertToRoman(num) {
     };
 
     let result = "";
-    for (const symbol in obj) {
-        while (decimal >= obj[symbol]) {
+    for (const symbol in lookup) {
+        while (decimal >= lookup[symbol]) {
             result += symbol;
-            decimal -= obj[symbol];
+            decimal -= lookup[symbol];
         }
     }
     return result;
