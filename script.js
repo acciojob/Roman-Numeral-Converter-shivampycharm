e61dff39e1812c355ff010f6dbd773b072b969e8
Shivam Dubey <72991053+shivampycharm@users.noreply.github.com>
2023-01-12 19:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
 function convertToRoman(num) {
 
-	// const obj = {
- //        M: 1000,
- //        CM: 900,
- //        D: 500,
- //        CD: 400,
- //        C: 100,
- //        XC: 90,
- //        L: 50,
- //        XL: 40,
- //        X: 10,
- //        IX: 9,
- //        V: 5,
- //        IV: 4,
- //        I: 1
- //    };
-
- //    let result = "";
- //    for (const symbol in obj) {
- //        while (decimal >= obj[symbol]) {
- //            result += symbol;
- //            decimal -= obj[symbol];
- //        }
- //    }
- //    return result;
-
-	const lookup = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        I: 1
-    };
-
-    let result = "";
-    for (const symbol in lookup) {
-        while (decimal >= lookup[symbol]) {
-            result += symbol;
-            decimal -= lookup[symbol];
-        }
-    }
-    return result;
-
+	const map = {
+    M:  1000,
+    CM: 900,
+    D:  500,
+    CD: 400,
+    C:  100,
+    XC: 90,
+    L:  50,
+    XL: 40,
+    X:  10,
+    IX: 9,
+    V:  5,
+    IV: 4,
+    I:  1,
+  };  
+  let result = '';
+  
+  for (key in map) {
+    result += key.repeat(Math.floor(num / map[key]));
+    num %= map[key];
+  }
+  
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
